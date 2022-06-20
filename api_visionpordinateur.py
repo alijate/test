e125a9e3ece06588e2c2ce8be97d4f4d056d8b7f
alijate <64009369+alijate@users.noreply.github.com>
2022-06-20 23:04:13 +0100

    Create api_visionpordinateur.py

---
+++ api_visionpordinateur.py
@@ -0,0 +1,18 @@
+# Create a dataframe with the image URLs
+df = spark.createDataFrame([
+        ("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/objects.jpg", ),
+        ("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/dog.jpg", ),
+        ("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/house.jpg", )
+    ], ["image", ])
+
+# Run the Computer Vision service. Analyze Image extracts infortmation from/about the images.
+analysis = (AnalyzeImage()
+    .setLocation("eastus")
+    .setSubscriptionKey(service_key)
+    .setVisualFeatures(["Categories","Color","Description","Faces","Objects","Tags"])
+    .setOutputCol("analysis_results")
+    .setImageUrlCol("image")
+    .setErrorCol("error"))
+
+# Show the results of what you wanted to pull out of the images.
+display(analysis.transform(df).select("image", "analysis_results.description.tags"))
