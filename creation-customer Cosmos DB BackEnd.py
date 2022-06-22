c6c27d7de3a4b4e4aa2224c2e33cb1af352a69b8
alijate <64009369+alijate@users.noreply.github.com>
2022-06-22 16:50:55 +0100

    Add files via upload

---
+++ creation-customer Cosmos DB BackEnd.py
@@ -0,0 +1,8 @@
+#creation du  the CosmosClient
+
+from azure.cosmos import CosmosClient
+
+import os
+URL = os.environ['ACCOUNT_URI']
+KEY = os.environ['ACCOUNT_KEY']
+client = CosmosClient(URL, credential=KEY)
