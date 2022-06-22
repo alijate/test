04ddc540004cca3d38a42c06c95ae583a6899c74
alijate <64009369+alijate@users.noreply.github.com>
2022-06-22 18:06:49 +0100

    Delete creation-customer Cosmos DB BackEnd.py

--- creation-customer Cosmos DB BackEnd.py
+++
@@ -1,8 +0,0 @@
-#creation du  the CosmosClient
-
-from azure.cosmos import CosmosClient
-
-import os
-URL = os.environ['ACCOUNT_URI']
-KEY = os.environ['ACCOUNT_KEY']
-client = CosmosClient(URL, credential=KEY)
